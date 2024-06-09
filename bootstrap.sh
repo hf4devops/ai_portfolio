2524e161a82bded60d2729ba6df2c10e5fba4c48
hf4devops <hermfehl@gmail.com>
2024-06-09 20:07:46 +0200

    OpenAI api works

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "###" ~/.bashrc
+echo "export LANGCHAIN_TRACING_V2=true" >> ~/.bashrc
+echo "export LANGCHAIN_API_KEY=$(cat api.key)" >> ~/.bashrc
+echo "export OPENAI_API_KEY=$(cat openapi.key)" >> ~/.bashrc
+
+
+pip install -r ai_portfolio/requirements.txt
+source ~/.bashrc
